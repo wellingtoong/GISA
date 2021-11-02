8571b134c09eb1093398346ee0f38821c03eadc8
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2021-11-02 10:48:47 -0300

    Criação das entidades de dominio Convenio e Endereço

---
+++ src/services/GISA.Convenio.API/Domain/Endereco.cs
@@ -0,0 +1,27 @@
+﻿using GISA.Core.DomainObjects;
+
+namespace GISA.Convenio.API.Domain
+{
+    public class Endereco : Entity
+    {
+        public string Cep { get; private set; }
+        public string Logradouro { get; private set; }
+        public string Numero { get; private set; }
+        public string Complemento { get; private set; }
+        public string Bairro { get; private set; }
+        public string Estado { get; private set; }
+        public string Municipio { get; private set; }
+
+        protected Endereco() { }
+
+        public Endereco(string cep, string logradouro, string numero, string bairro, string estado, string municipio)
+        {
+            Cep = cep;
+            Logradouro = logradouro;
+            Numero = numero;
+            Bairro = bairro;
+            Estado = estado;
+            Municipio = municipio;
+        }
+    }
+}
