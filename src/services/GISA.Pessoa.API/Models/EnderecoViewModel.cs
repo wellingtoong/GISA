5c2a9bdd38a0428c311ca3f17eea2bfe03a6a075
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2021-11-07 17:34:36 -0300

    ViewModels das entidades

---
+++ src/services/GISA.Pessoa.API/Models/EnderecoViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace GISA.Pessoa.API.Models
+{
+    public class EnderecoViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [MaxLength(8, ErrorMessage = "O campo {0} pode ter no máximo {1} caracteres.")]
+        public string Cep { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [MaxLength(200, ErrorMessage = "O campo {0} pode ter no máximo {1} caracteres.")]
+        public string Logradouro { get; set; }
+
+        [DisplayName("Número")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [MaxLength(50, ErrorMessage = "O campo {0} pode ter no máximo {1} caracteres.")]
+        public string Numero { get; set; }
+
+        [MaxLength(200, ErrorMessage = "O campo {0} pode ter no máximo {1} caracteres.")]
+        public string Complemento { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [MaxLength(100, ErrorMessage = "O campo {0} pode ter no máximo {1} caracteres.")]
+        public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [MaxLength(2, ErrorMessage = "O campo {0} pode ter no máximo {1} caracteres.")]
+        public string Estado { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [MaxLength(200, ErrorMessage = "O campo {0} pode ter no máximo {1} caracteres.")]
+        public string Municipio { get; set; }
+
+    }
+}
