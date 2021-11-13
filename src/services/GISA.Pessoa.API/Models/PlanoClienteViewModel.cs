a897351e784c561c9a9147b22e1bb9fd9a84ffe0
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2021-11-13 16:55:00 -0300

    Criado classe PlanoClienteViewModel

---
+++ src/services/GISA.Pessoa.API/Models/PlanoClienteViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GISA.Pessoa.API.Models
+{
+    public class PlanoClienteViewModel
+    {
+        [Key]
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public Guid PessoaId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public Guid PlanoId { get; set; }
+
+        public int Acrescimo { get; set; }
+
+        public int Desconto { get; set; }
+
+        public decimal ValorFinal { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+    }
+}
