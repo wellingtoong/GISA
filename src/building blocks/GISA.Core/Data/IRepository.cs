187ea3c0cab373030ab22626bbae6bb6df805cb0
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2021-12-10 22:46:04 -0300

    Adicionado interface IRepository para uso global

---
+++ src/building blocks/GISA.Core/Data/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using GISA.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace GISA.Core.Data
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+        Task<bool> Adicionar(TEntity entity);
+        Task<TEntity> ObterPorId(Guid id);
+        Task<List<TEntity>> ObterTodos();
+        Task<bool> Atualizar(TEntity entity);
+        Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate);
+        Task<bool> SaveChanges();
+    }
+}
