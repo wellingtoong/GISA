fde61a526e599ad1add3790307fb711c65c7e7d7
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2022-01-23 19:30:51 -0300

    Criado interface para integração de PlanoCliente

--- src/services/GISA.Pessoa.API/Domain/PlanoCliente.cs
+++ src/services/GISA.Pessoa.API/Domain/PlanoCliente.cs
@@ -7,11 +7,19 @@ namespace GISA.Pessoa.API.Domain
     {
         public Guid PessoaId { get; private set; }
         public Guid PlanoId { get; private set; }
-        public int? Acrescimo { get; private set; }
+        //public int? Acrescimo { get; private set; }
         public int? Desconto { get; private set; }
         public decimal ValorFinal { get; private set; }
         public DateTime DataCadastro { get; private set; }
 
-        public PlanoCliente() { }
+        protected PlanoCliente() { }
+
+        public PlanoCliente(Guid pessoaId, Guid planoId, int desconto, decimal valorFinal)
+        {
+            PessoaId = pessoaId;
+            PlanoId = planoId;
+            Desconto = desconto;
+            ValorFinal = valorFinal;
+        }
     }
 }
