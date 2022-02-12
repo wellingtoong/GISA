4af8236993b426966faba5f9e89faa9b78dd54dd
Wellington Gonzalez <wellington.gonzalez@hotmail.com>
2022-02-12 19:27:46 -0300

    Criação pagina de agendamento e vinculação do plugin datepiker

--- src/web/GISA.WebApp.MVC/Controllers/AgendaController.cs
+++ src/web/GISA.WebApp.MVC/Controllers/AgendaController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
 
 namespace GISA.WebApp.MVC.Controllers
 {
@@ -7,6 +9,75 @@ namespace GISA.WebApp.MVC.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Route("agendamento/obter-todos")]
+        public async Task<IActionResult> ObterTodos()
+        {
+            return View();
+        }
+
+        [Route("agendamento/editar-agenda/{id:guid}")]
+        public async Task<IActionResult> Editar(Guid id)
+        {
+            //var pessoa = await _pessoaService.ObterPorId(id);
+            //return View(pessoa);
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route("agendamento/editar-pessoa")]
+        public async Task<IActionResult> Editar(/*Guid id, AgendaViewModel agendaViewModel*/)
+        {
+            //var pessoa = await _pessoaService.ObterPorId(id);
+            //return View(pessoa);
+
+            return View();
+        }
+
+        [Route("agendamento/novo-agenda")]
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //[Route("agendamento/novo-agenda")]
+        //public async Task<IActionResult> Registrar(/*AgendaViewModel agendaViewModel*/)
+        //{
+        //if (!ModelState.IsValid)
+        //{
+        //    ViewBag.ValidateForm = true;
+        //    return View(pessoaViewModel);
+        //}
+
+        //var result = await _pessoaService.Registrar(pessoaViewModel);
+
+        //if (!result.Sucess)
+        //{
+        //    // TODO: faço algo
+        //}
+
+        //    return View("Index");
+        //}
+
+        public async Task<IActionResult> Atualizar(/*Guid id, PessoaViewModel pessoaViewModel*/)
+        {
+            //if (!ModelState.IsValid)
+            //{
+            //    ViewBag.ValidateForm = true;
+            //    return View("Editar", pessoaViewModel);
+            //}
+
+            //var result = await _pessoaService.Atualizar(pessoaViewModel);
+
+            //if (!result.Sucess)
+            //{
+            //    // TODO: faço algo
+            //}
+
+            return View("Index");
         }
     }
 }
